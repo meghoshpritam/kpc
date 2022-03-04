6654a187ad939e9d6447f89cd6f5cf0c5a7c11c1
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2022-03-04 10:47:17 +0530

    reformat codebase

--- themes/psicologoscolastico-theme/assets/js/app.js
+++ themes/psicologoscolastico-theme/assets/js/app.js
@@ -1,3 +1,5 @@
+import Utils from './utils';
+
 const SCROLL_HEADER_SELECTOR = '.page-header-0203';
 const RELATIVE_HEADER_SELECTOR = '.page-relative-header-0203';
 const MENU_DRAWER_SELECTOR = '.menu-drawer-0303';
@@ -5,276 +7,11 @@ const TAG_BLOG_CONTAINER_SELECTOR = '[data-id="blog-by-tag-0303"]';
 const TAG_BUTTON_SELECTOR = '[data-id="tag-selector-button-0303"]';
 const HIDDEN_CLASS = 'hidden';
 
-class Utils {
-  static disableButtonClass = 'disable-card-button';
-
-  //  fix the card container height
-  static articleCarsInit = ({ selector, wrapperSelector, nextButton, lineSelector, numberSelector }) => {
-    const cards = document.querySelectorAll(selector);
-    let maxHeight = 0;
-    let displayed = 0;
-    let lastDisplayed = -1;
-
-    for (let idx = 0; idx < cards.length; idx++) {
-      let cDisplay = window.getComputedStyle(cards[idx], null).display;
-
-      if (cDisplay !== 'none') {
-        lastDisplayed = idx;
-        displayed += 1;
-      }
-
-      cards[idx].style.display = 'grid';
-      if (maxHeight < cards[idx].clientHeight) maxHeight = cards[idx].clientHeight;
-
-      cards[idx].style.display = cDisplay;
-    }
-
-    // disable next button if all cards are displayed initially
-    if (cards.length === displayed) document.querySelector(nextButton).classList.add(Utils.disableButtonClass);
-
-    document.querySelector(numberSelector).innerHTML = `${lastDisplayed + 1}`;
-    document.querySelector(lineSelector).style.width = `${Math.ceil((100 * (lastDisplayed + 1)) / cards.length)}%`;
-    document.querySelector(wrapperSelector).style.minHeight = `${maxHeight}px`;
-  };
-
-  // initialize height and dot of cars
-  static initDotCards = ({ selector, containerSelector, nextButton, indicatorsSelector }) => {
-    const cards = document.querySelectorAll(selector);
-
-    let maxHeight = 0;
-    let displayed = 0;
-    let lastDisplayed = -1;
-
-    for (let idx = 0; idx < cards.length; idx++) {
-      let cDisplay = window.getComputedStyle(cards[idx], null).display;
-
-      if (cDisplay !== 'none') {
-        lastDisplayed = idx;
-        displayed += 1;
-      }
-
-      cards[idx].style.display = 'flex';
-      if (maxHeight < cards[idx].clientHeight) maxHeight = cards[idx].clientHeight;
-
-      cards[idx].style.display = cDisplay;
-    }
-
-    // disable next button if all cards are displayed initially
-    if (cards.length === displayed) document.querySelector(nextButton).classList.add(Utils.disableButtonClass);
-
-    // get all the indicators
-    const indicators = document.querySelectorAll(indicatorsSelector);
-
-    for (let idx = 0; idx < indicators.length; idx++) {
-      if (idx < Math.ceil(cards.length / displayed)) indicators[idx].classList.remove('hidden');
-      else {
-        indicators[idx].classList.add('hidden');
-      }
-    }
-
-    // hr height
-    let hrHeight = 0;
-    const hrs = document.querySelectorAll(containerSelector + ' > hr');
-
-    for (let idx = 0; idx < hrs.length; idx++) {
-      if (hrHeight < window.getComputedStyle(hrs[idx]).height) hrHeight = window.getComputedStyle(hrs[idx]).height;
-    }
-
-    for (let idx = 0; idx < cards.length; idx++) {
-      cards[idx].style.minHeight = `${maxHeight}px`;
-    }
-    // document.querySelector(containerSelector).style.minHeight = `${2 * maxHeight + hrHeight}px`;
-  };
-  // change cards
-  static changeContentButton = ({
-    selector,
-    direction = 'next',
-    previousButton,
-    nextButton,
-    lineSelector,
-    numberSelector,
-  }) => {
-    const cards = document.querySelectorAll(selector);
-    let currentScreenCards = [];
-
-    for (let idx = 0; idx < cards.length; idx++) {
-      if (window.getComputedStyle(cards[idx], null).display !== 'none') {
-        currentScreenCards = [...currentScreenCards, idx];
-      }
-    }
-
-    const changeNewState = (states = [], add, remove) => {
-      for (let idx = 0; idx < cards.length; idx += 1) {
-        document.querySelector(previousButton).classList.add(Utils.disableButtonClass);
-        if (states[0] !== 0) document.querySelector(previousButton).classList.remove(Utils.disableButtonClass);
-
-        document.querySelector(nextButton).classList.add(Utils.disableButtonClass);
-        if (states[states.length - 1] !== cards.length - 1)
-          document.querySelector(nextButton).classList.remove(Utils.disableButtonClass);
-
-        document.querySelector(numberSelector).innerHTML = `${states[states.length - 1] + 1}`;
-        document.querySelector(lineSelector).style.width = `${Math.ceil(
-          (100 * (states[states.length - 1] + 1)) / cards.length,
-        )}%`;
-
-        if (states.indexOf(idx) !== -1) {
-          cards[idx].classList.remove('animate__animated', `animate__fadeIn${remove}`);
-          cards[idx].classList.add('animate__animated', `animate__fadeIn${add}`);
-          cards[idx].style.display = 'grid';
-        } else {
-          cards[idx].style.display = 'none';
-        }
-      }
-    };
-
-    if (direction === 'next') {
-      let newScreenCards = [];
-      let newLastNum = currentScreenCards[currentScreenCards.length - 1] + currentScreenCards.length;
-
-      if (newLastNum >= cards.length) newLastNum = cards.length - 1;
-
-      for (let idx = currentScreenCards.length - 1; idx > -1; idx -= 1) {
-        newScreenCards = [...newScreenCards, newLastNum - idx];
-      }
-
-      changeNewState(newScreenCards, 'Right', 'Left');
-    } else if (direction === 'previous') {
-      let newScreenCards = [];
-
-      let newFirstNum = currentScreenCards[0] - currentScreenCards.length;
-
-      if (newFirstNum < 0) newFirstNum = 0;
-
-      for (let idx = 0; idx < currentScreenCards.length; idx += 1) {
-        newScreenCards = [...newScreenCards, newFirstNum + idx];
-      }
-
-      changeNewState(newScreenCards, 'Left', 'Right');
-    }
-  };
-
-  // dot card change button
-  static dotCardChangeButton = ({
-    selector,
-    direction = 'next',
-    previousButton,
-    nextButton,
-    indicatorsSelector,
-    containerSelector,
-    position,
-  }) => {
-    const cards = document.querySelectorAll(selector);
-    let currentScreenCards = [];
-
-    for (let idx = 0; idx < cards.length; idx++) {
-      if (window.getComputedStyle(cards[idx], null).display !== 'none') {
-        currentScreenCards = [...currentScreenCards, idx];
-      }
-    }
-
-    const changeNewState = (states = [], add, remove) => {
-      // change the hr tag
-      if (Number(window.getComputedStyle(document.querySelector('body')).width.split('px')[0]) >= 768) {
-        const hrs = document.querySelectorAll(containerSelector + ' > hr');
-
-        for (let index = 0; index < hrs.length; index++) {
-          if (index === states[states.length - 1] - 1) {
-            hrs[index].style.display = 'block';
-          } else {
-            hrs[index].style.display = 'none';
-          }
-        }
-      }
-
-      // highlight the current indicator
-      const indicators = document.querySelectorAll(indicatorsSelector);
-      for (let idx = 0; idx < indicators.length; idx++) {
-        if (idx === Math.floor(states[states.length - 1] / states.lenguh)) {
-          indicators[idx].classList.remove('shadow-sm', 'bg-ice-blue2');
-          indicators[idx].classList.add('shadow-lg', 'bg-g4');
-        } else {
-          indicators[idx].classList.remove('shadow-lg', 'bg-g4');
-          indicators[idx].classList.add('shadow-sm', 'bg-ice-blue2');
-        }
-      }
-
-      for (let idx = 0; idx < cards.length; idx += 1) {
-        document.querySelector(previousButton).classList.add(Utils.disableButtonClass);
-        if (states[0] !== 0) document.querySelector(previousButton).classList.remove(Utils.disableButtonClass);
-
-        document.querySelector(nextButton).classList.add(Utils.disableButtonClass);
-        if (states[states.length - 1] !== cards.length - 1)
-          document.querySelector(nextButton).classList.remove(Utils.disableButtonClass);
-
-        // document.querySelector(numberSelector).innerHTML = `${states[states.length - 1] + 1}`;
-        // document.querySelector(lineSelector).style.width = `${Math.ceil(100 * (states[states.length - 1] + 1) / (cards.length))}%`;
-
-        if (states.indexOf(idx) !== -1) {
-          cards[idx].classList.remove('animate__animated', `animate__fadeIn${remove}`);
-          cards[idx].classList.add('animate__animated', `animate__fadeIn${add}`);
-          cards[idx].style.display = 'flex';
-        } else {
-          cards[idx].style.display = 'none';
-        }
-      }
-    };
-
-    if (position) {
-      let max = position * currentScreenCards.length - 1;
-
-      if (max > cards.length - 1) max = cards.length - 1;
-
-      if (max - currentScreenCards.length - 1 < 0) {
-        max = currentScreenCards.length - 1;
-      }
-
-      let newStates = [];
-      for (let idx = currentScreenCards.length - 1; idx > -1; idx -= 1) {
-        newStates = [...newStates, max - idx];
-      }
-
-      // check previous or next move by the position difference
-      let next = false;
-      for (let idx = 0; idx < currentScreenCards.length; idx += 1) {
-        if (currentScreenCards[idx] < newStates[idx]) {
-          next = true;
-        }
-      }
-
-      if (next) changeNewState(newStates, 'Right', 'Left');
-      else changeNewState(newStates, 'Left', 'Right');
-    } else if (direction === 'next') {
-      let newScreenCards = [];
-      let newLastNum = currentScreenCards[currentScreenCards.length - 1] + currentScreenCards.length;
-
-      if (newLastNum >= cards.length) newLastNum = cards.length - 1;
-
-      for (let idx = currentScreenCards.length - 1; idx > -1; idx -= 1) {
-        newScreenCards = [...newScreenCards, newLastNum - idx];
-      }
-
-      changeNewState(newScreenCards, 'Right', 'Left');
-    } else if (direction === 'previous') {
-      let newScreenCards = [];
-
-      let newFirstNum = currentScreenCards[0] - currentScreenCards.length;
-
-      if (newFirstNum < 0) newFirstNum = 0;
-
-      for (let idx = 0; idx < currentScreenCards.length; idx += 1) {
-        newScreenCards = [...newScreenCards, newFirstNum + idx];
-      }
-
-      changeNewState(newScreenCards, 'Left', 'Right');
-    }
-  };
-}
-
 class App {
   static menu = false;
+
   // toggle menu btn
   static toggleMenu = () => {
-    const body = document.querySelector('body');
     const header = document.querySelector(SCROLL_HEADER_SELECTOR);
     let headerHeight = header.clientHeight;
 
@@ -304,6 +41,7 @@ class App {
       document.querySelector(SCROLL_HEADER_SELECTOR).classList.add('hidden');
     }
   };
+
   static onLoad = () => {
     // change current year
     document.querySelector('.current-year-footer-0102').innerHTML = new Date().getFullYear();
@@ -317,7 +55,7 @@ class App {
     let articleHeadings = [];
 
     if (tocLinks.length > 0)
-      for (let idx = 0, tocIdx = 0; idx < allArticleHeadings.length; idx++) {
+      for (let idx = 0, tocIdx = 0; idx < allArticleHeadings.length; idx += 1) {
         if (allArticleHeadings[idx]?.innerHTML === tocLinks[tocIdx]?.innerHTML) {
           articleHeadings = [...articleHeadings, allArticleHeadings[idx]];
           tocIdx += 1;
@@ -325,8 +63,8 @@ class App {
       }
 
     const fixTOCTop = () => {
-      const headerHeight = document.querySelector(SCROLL_HEADER_SELECTOR).clientHeight;
-      document.querySelector('.toc-container').style.top = `${headerHeight + 20}px`;
+      const _headerHeight = document.querySelector(SCROLL_HEADER_SELECTOR).clientHeight;
+      document.querySelector('.toc-container').style.top = `${_headerHeight + 20}px`;
     };
 
     fixTOCTop();
@@ -334,10 +72,8 @@ class App {
     let current = 0;
     const headerOffset = 50;
 
-    let distances = [];
-    for (let index = 0; index < articleHeadings.length; index += 1) {
-      distances = [...distances, articleHeadings[index].getBoundingClientRect().top - headerHeight];
-    }
+    const distances =
+      articleHeadings.map((articleHeading) => articleHeading.getBoundingClientRect().top - headerHeight) || [];
 
     distances.sort((a, b) => a - b);
 
@@ -368,7 +104,7 @@ class App {
     let lastScrollTop = 0;
 
     for (let index = 0; index < tocLinks.length; index += 1) {
-      tocLinks[index].addEventListener('click', function (event) {
+      tocLinks[index].addEventListener('click', (event) => {
         const header = document.querySelector(SCROLL_HEADER_SELECTOR);
         let headHeight = header.clientHeight;
 
@@ -389,25 +125,23 @@ class App {
       });
     }
 
-    window.addEventListener('scroll', function () {
-      const headerHeight = document.querySelector(SCROLL_HEADER_SELECTOR).clientHeight;
+    window.addEventListener('scroll', () => {
+      const _headerHeight = document.querySelector(SCROLL_HEADER_SELECTOR).clientHeight;
       fixTOCTop();
       hightLight(current);
       const st = window.pageYOffset || document.documentElement.scrollTop;
 
       if (st > lastScrollTop) {
         if (current < articleHeadings.length - 1) {
-          if (articleHeadings[current + 1].getBoundingClientRect().top < headerHeight + headerOffset) {
+          if (articleHeadings[current + 1].getBoundingClientRect().top < _headerHeight + headerOffset) {
             hightLight(current + 1);
             current += 1;
           }
         }
-      } else {
-        if (current >= 1) {
-          if (articleHeadings[current].getBoundingClientRect().top > headerHeight + headerOffset * 2) {
-            hightLight(current - 1);
-            current -= 1;
-          }
+      } else if (current >= 1) {
+        if (articleHeadings[current].getBoundingClientRect().top > _headerHeight + headerOffset * 2) {
+          hightLight(current - 1);
+          current -= 1;
         }
       }
       lastScrollTop = st <= 0 ? 0 : st;
@@ -534,7 +268,7 @@ class App {
     const mapCards = document.querySelectorAll('.map-selector-card2206');
 
     if (Number(select) > -1 && Number(select) < maps.length)
-      for (let idx = 0; idx < maps.length; idx++) {
+      for (let idx = 0; idx < maps.length; idx += 1) {
         if (Number(select) === idx) {
           maps[idx].classList.remove('hidden');
           mapCards[idx].classList?.remove('text-g4');
@@ -576,7 +310,7 @@ class App {
       modified = [...modified, false];
     });
 
-    window.addEventListener('scroll', function () {
+    window.addEventListener('scroll', () => {
       let increaseClassIdx = 0;
 
       const getClasses = () => {
@@ -602,18 +336,15 @@ class App {
             element.classList.add(...getClasses());
             modified[modifiedIdx] = true;
           }
-        } else {
-          if (!(remain && modified[modifiedIdx])) element.classList.remove(...getClasses());
-        }
+        } else if (!(remain && modified[modifiedIdx])) element.classList.remove(...getClasses());
         modifiedIdx += 1;
         increaseClassIdx += 1;
       });
     });
   };
 
-  static getBlogContainerElementByTag = (tag) => {
-    return document.querySelector(`${TAG_BLOG_CONTAINER_SELECTOR}[data-tag="${tag}"]`);
-  };
+  static getBlogContainerElementByTag = (tag) =>
+    document.querySelector(`${TAG_BLOG_CONTAINER_SELECTOR}[data-tag="${tag}"]`);
 
   static hideAllElements = (elements) => {
     for (const element of elements) {
@@ -651,7 +382,7 @@ class App {
   };
 
   static searchBlog = (event) => {
-    const target = event.target;
+    const { target } = event;
     const searchTerm = target.value || '';
     const searchTerms = searchTerm
       .toLowerCase()
