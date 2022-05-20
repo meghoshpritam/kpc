35c56ee8ff172c39645522a0b310bbf8ff660043
Pritam <contact.meghoshpritam@gmail.com>
2022-05-20 13:51:16 +0530

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 git clone git@github.com:meghoshpritam/kpc.git build
 cd build
+rm -rf public
 git checkout build-prod
-cp ../public ./ -r
+cp ../public ./public -r
 git commit -m "new update"
 git push origin -u build-prod
 cd ..
